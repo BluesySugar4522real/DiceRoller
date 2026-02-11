be3060333046d9382939b16b92aba3a9a1f49b25
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-02-11 17:47:28 +0530

    Refactor guess function to include play again prompt

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function guess() {
     let guess = prompt("Guess the number on the dice (1-6)");
 
     if (guess == random) {
-        document.getElementById("txt").innerHTML = "You guessed right! The number was " + random;
+        document.getElementById("txt").innerHTML = "You guessed right! The number was " + random + ". Play again?";
     } else {
         document.getElementById("txt").innerHTML = "You guessed wrong! The number was " + random + ". Try again!";
     }
+
 }
